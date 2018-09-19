cc97c2a4f53f13f7dd9441133912ca92d64c570d
andric <rodomir3119841985@gmail.com>
2018-09-19 20:14:27 +0300

    'header'

--- public/js/jscripts.js
+++ public/js/jscripts.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-    
-    // Hidden menu
+// $(document).ready(function() {
+//
+//     // Hidden menu
     $( '.btn-menu, .hidden-menu ul a' ).click( function() {
         if ( $( '.hidden-menu' ).is( ':hidden' ) ) {
             $( '.hidden-menu' ).show();
@@ -8,23 +8,30 @@ $(document).ready(function() {
             $( '.hidden-menu' ).hide();
         }
     });
-    
+//
     // Page scroll
     $("a[rel='m_PageScroll2id']").mPageScroll2id({
         offset:30
     });
-    
-    // Slider
-    $("#owl-demo").owlCarousel({
-        autoPlay: 3000, //Set AutoPlay to 3 seconds
-        items : 3,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3]
-    });
-    
-    // Tabs
-    $('#responsiveTabsDemo').responsiveTabs({
-        startCollapsed: 'tabs'
-    });
-    
-});
+//
+//     // Slider
+//     $("#owl-demo").owlCarousel({
+//         autoPlay: 3000, //Set AutoPlay to 3 seconds
+//         items : 3,
+//         itemsDesktop : [1199,3],
+//         itemsDesktopSmall : [979,3]
+//     });
+//
+//     // Tabs
+//     $('#responsiveTabsDemo').responsiveTabs({
+//         startCollapsed: 'tabs'
+//     });
+//
+// });
+///burger
+// $('.menu-btn').on('click', function(e) {
+//     e.preventDefault;
+//     $(this).toggleClass('menu-btn_active');
+//     $('#left_side').toggleClass('open');
+//
+// });
