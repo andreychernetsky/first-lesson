461588b3660c5c36f58216986605654d0f3f2ed6
andric <rodomir3119841985@gmail.com>
2018-09-20 22:47:56 +0300

    slider

--- public/js/jscripts.js
+++ public/js/jscripts.js
@@ -13,5 +13,16 @@
      $("a[rel='m_PageScroll2id']").mPageScroll2id({
          offset: 30
      });
+     // Slider
+     $("#owl-demo").owlCarousel({
+         autoPlay: 3000, //Set AutoPlay to 3 seconds
+         items : 4,
+         itemsDesktop : [500,5],
+         itemsDesktopSmall : [200,5]
+     });
+     // Tabs
+     $('#responsiveTabsDemo').responsiveTabs({
+         startCollapsed: 'tabs'
+     });
  });
 
